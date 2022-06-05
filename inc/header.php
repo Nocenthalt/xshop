3db7552c1e95fc9f6cbc7f06abe29064d0ec222e
Maladroy <axhaltz@gmail.com>
2022-06-05 11:26:46 +0700

    Updated CSS files Added Font Awesome Added Tailwind CSS

--- inc/header.php
+++ inc/header.php
@@ -2,18 +2,14 @@
     <head>
         <title> Hệ thống điều hướng cơ bản</title>
         <link href="/public/css/reset.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="header.css">
         <link rel="stylesheet" type="text/css" href="/public/css/style.css"/>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://kit.fontawesome.com/33c0badbf8.js" crossorigin="anonymous"></script> 
     </head>
     <body>
         <div id="wrapper">
             <div id="header">
-                <ul id="main_menu">
-                    <li><a href="?page=home">Trang chủ</a></li>
-                    <li><a href="?page=about">Giới thiệu</a></li>
-                    <li><a href="?page=news">Tin tức</a></li>
-                    <li><a href="?page=product">sản phẩm</a></li>
-                    <li><a href="?page=contact">Liên hệ</a></li>
-                    <li><a href="?page=course">khóa học</a></li>
-                    <li><a href="?page=ha">Trang Ha</a></li>
+            
             </div>
             <!--End_header-->
